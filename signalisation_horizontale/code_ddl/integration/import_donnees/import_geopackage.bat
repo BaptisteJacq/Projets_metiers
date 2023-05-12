df03b6021df4d0336ef899b11b3d63812d1fa64c
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2023-05-12 09:14:35 +0200

    maj du code

--- signalisation_horizontale/code_ddl/integration/import_donnees/import_geopackage.bat
+++ signalisation_horizontale/code_ddl/integration/import_donnees/import_geopackage.bat
@@ -7,17 +7,17 @@ SET /p MDP_D="Veuillez saisir le mot de passe de l'utilisateur Oracle de destina
 SET /p INSTANCE_D="Veuillez saisir l'instance Oracle de destination: "
 
 :: 2. se mettre dans l'environnement QGIS
-cd C:\Program Files\QGIS 3.24.2\bin
+cd C:\Program Files\QGIS 3.28.5\bin
 
 :: 3. Configurer le système d'encodage des caractères en UTF-8
 SET NLS_LANG=AMERICAN_AMERICA.AL32UTF8
 
 :: 4. Rediriger la variable PROJ_LIB vers le bon fichier proj.db afin qu'ogr2ogr trouve le bon scr
-setx PROJ_LIB "C:\Program Files\QGIS 3.24.2\share\proj"
+setx PROJ_LIB "C:\Program Files\QGIS 3.28.5\share\proj"
 
 :: 5. commande ogr2ogr pour exporter les couches du geopackage X@X vers le schéma X@X
-::ogr2ogr -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% C:\Users\bjacq\Documents\Projets\signalisation_horizontale\MEL_SH_2022_catalogage.gpkg -sql "SELECT GEOM, id_sh_longitudinale, modulation, largeur, registre, etat, nature_materiau, remarque, num_troncon, num_iti_cyclable, auteur, couleur FROM MEL_sh_longitudinale" -nln TEMP_SIGNALISATION_MEL_SH_LONGITUDINALE -nlt MULTILINESTRING -lco SRID=2154 -dim 2 -skipfailures
-ogr2ogr -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% C:\Users\bjacq\Documents\Projets\signalisation_horizontale\MEL_SH_2022_catalogage.gpkg -sql "SELECT GEOM, id_marquage_securite, nom_image, registre, etat, nature_materiau, remarque, auteur, num_troncon, num_iti_cyclable FROM MEL_sh_marq_securite" -nln TEMP_SIGNALISATION_MEL_SH_MARQ_SECURITE -nlt MULTILINESTRING -lco SRID=2154 -dim 2 -skipfailures
+ogr2ogr -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% C:\Users\bjacq\Documents\Projets\signalisation_horizontale\MEL_SH_2022_catalogage.gpkg -sql "SELECT GEOM, id_sh_longitudinale, modulation, largeur, registre, etat, nature_materiau, remarque, num_troncon, num_iti_cyclable, auteur, couleur FROM MEL_sh_longitudinale" -nln TEMP_SIGNALISATION_MEL_SH_LONGITUDINALE -nlt MULTILINESTRING -lco SRID=2154 -dim 2 -skipfailures
+::ogr2ogr -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% C:\Users\bjacq\Documents\Projets\signalisation_horizontale\MEL_SH_2022_catalogage.gpkg -sql "SELECT GEOM, id_marquage_securite, nom_image, registre, etat, nature_materiau, remarque, auteur, num_troncon, num_iti_cyclable FROM MEL_sh_marq_securite" -nln TEMP_SIGNALISATION_MEL_SH_MARQ_SECURITE -nlt MULTILINESTRING -lco SRID=2154 -dim 2 -skipfailures
 ::ogr2ogr -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% C:\Users\bjacq\Documents\Projets\signalisation_horizontale\MEL_SH_2022_catalogage.gpkg -sql "SELECT GEOM, id_place_pmr, nom_image, etat, nature_materiau, angle_nord, remarque, auteur, registre FROM MEL_sh_place_pmr" -nln TEMP_SIGNALISATION_MEL_SH_PLACE_PMR -nlt POINT -lco SRID=2154 -dim 2 -skipfailures
 ::ogr2ogr -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% C:\Users\bjacq\Documents\Projets\signalisation_horizontale\MEL_SH_2022_catalogage.gpkg -sql "SELECT GEOM, id_sas_cyclable, nom_image, etat, nature_materiau, angle_nord, remarque, auteur, registre FROM MEL_sh_sas_cyclable" -nln TEMP_SIGNALISATION_MEL_SH_SAS_CYCLABLE -nlt POINT -lco SRID=2154 -dim 2 -skipfailures
 ::ogr2ogr -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% C:\Users\bjacq\Documents\Projets\signalisation_horizontale\MEL_SH_2022_catalogage.gpkg -sql "SELECT GEOM, id_ilot_marque, etat, registre, nature_materiau, nom_image, auteur, num_troncon, num_iti_cyclable, surface, couleur FROM MEL_sh_surf_marque" -nln TEMP_SIGNALISATION_MEL_SH_SURF_MARQUE -nlt MULTIPOLYGON -lco SRID=2154 -dim 2 -skipfailures
